da57fc2f19e64e9d95aa9ca5ccb63cd8096669fe
dev-cool <cdev9382@gmail.com>
2018-11-03 16:11:21 -0300

    [Changed] Layout of Login Screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,32 +1,86 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
+
+import { PRODUCTS_SCREEN, startSingleScreenApp } from './';
+import { SocialButton } from '../components';
+
+import colors from '../colors';
 import fonts from '../fonts';
 import i18n from '../i18n';
+import imgAppIcon from '../../assets/images/app-icon.png';
+import imgGoogleIcon from '../../assets/images/google-icon.png';
+
 
 type Props = {};
 class LoginScreen extends Component<Props> {
 
+  static navigatorStyle = {
+    navBarHidden: true
+  };
+
+  onPressButton() {
+    startSingleScreenApp(PRODUCTS_SCREEN);
+  }
+
   render() {
+    const {
+      containerStyle,
+      welcomeViewStyle,
+      imageStyle,
+      titleStyle,
+      buttonStyle
+    } = styles;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>{i18n.t('title')}</Text>
+      <View style={containerStyle}>
+        <View style={welcomeViewStyle}>
+          <Image style={imageStyle} source={imgAppIcon} />
+
+          <Text style={titleStyle}>
+            {i18n.t('login.welcome')}
+          </Text>
+        </View>
+
+        <SocialButton
+          style={buttonStyle}
+          color={colors.red}
+          icon={imgGoogleIcon}
+          title={i18n.t('login.button')}
+          onPress={() => this.onPressButton()}
+        />
       </View>
     );
   }
 
 }
 
+const margin = 14;
 const styles = StyleSheet.create({
-  container: {
+  containerStyle: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
-    paddingHorizontal: 10,
-    paddingVertical: 30
+    alignItems: 'center',
+    backgroundColor: colors.while,
+    margin
   },
-  title: {
+  welcomeViewStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imageStyle: {
+    height: 100,
+    width: 100,
+    margin
+  },
+  titleStyle: {
     fontFamily: fonts.regular,
-    fontSize: 20,
-    marginBottom: 10
+    color: colors.black,
+    fontSize: 24,
+    margin
+  },
+  buttonStyle: {
+    position: 'absolute',
+    bottom: 0
   }
 });
 
