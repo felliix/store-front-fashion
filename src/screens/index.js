5f6d40980a241f660c26e935942a3a58632603d1
dev-cool <cdev9382@gmail.com>
2018-11-03 18:57:52 -0300

    [Changed] Navigator Style and Register Component

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,9 +2,8 @@ import DeviceInfo from 'react-native-device-info';
 import { Navigation } from 'react-native-navigation';
 
 import LoginScreen from './LoginScreen';
+import ProductScreen from './ProductScreen';
 import ProductsScreen from './ProductsScreen';
-
-import { NAVBAR_VIEW } from '../components';
 
 import colors from '../colors';
 import fonts from '../fonts';
@@ -14,10 +13,12 @@ import imgAppBack from '../../assets/images/app-back.png';
 // Public
 
 export const LOGIN_SCREEN = screenUniqueName('LoginScreen');
+export const PRODUCT_SCREEN = screenUniqueName('ProductScreen');
 export const PRODUCTS_SCREEN = screenUniqueName('ProductsScreen');
 
 export function registerScreens() {
   Navigation.registerComponent(LOGIN_SCREEN, () => LoginScreen);
+  Navigation.registerComponent(PRODUCT_SCREEN, () => ProductScreen);
   Navigation.registerComponent(PRODUCTS_SCREEN, () => ProductsScreen);
 }
 
@@ -57,14 +58,14 @@ const appStyle = {
 
 const navigatorStyle = {
   // Android and iOS
-  navBarTextFontFamily: fonts.regular,
-  navBarTextFontSize: 18,
+  navBarTextFontFamily: fonts.bold,
+  navBarTextFontSize: 22,
   navBarBackgroundColor: colors.white,
-  navBarButtonColor: colors.black,
-  navBarCustomView: NAVBAR_VIEW,
-  topBarElevationShadowEnabled: false,
+  navBarButtonColor: colors.grayDark,
+  topBarElevationShadowEnabled: true,
   navBarNoBorder: true,
   // Android only
-  navBarHeight: 53,
-  navBarTitleTextCentered: true
+  navBarTitleTextCentered: true,
+  // iOS only
+  largeTitle: true
 };
