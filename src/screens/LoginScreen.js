352c98cd9e9de0419ef1cba389b26d6a852133a0
dev-cool <cdev9382@gmail.com>
2018-11-04 14:26:29 -0200

    [Added] Log in action

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import { Alert, StyleSheet, Image, Text, View } from 'react-native';
 
 import { PRODUCTS_SCREEN, startSingleScreenApp } from './';
 import { SocialButton } from '../components';
+
+import GoogleService from '../services/GoogleService';
 
 import colors from '../colors';
 import fonts from '../fonts';
@@ -19,7 +21,24 @@ export default class LoginScreen extends Component<Props> {
   };
 
   onPressButton() {
-    startSingleScreenApp(PRODUCTS_SCREEN, 'fade');
+    GoogleService.signIn()
+      .then(() => {
+        setTimeout(() => {
+          startSingleScreenApp(PRODUCTS_SCREEN, 'fade');
+        }, 250);
+      })
+      .catch(error => {
+        if (error) {
+          Alert.alert(
+            i18n.t('app.attention'),
+            i18n.t('login.enter.message'),
+            [
+              { text: i18n.t('app.ok') },
+            ],
+            { cancelable: true }
+          );
+        }
+      });
   }
 
   render() {
