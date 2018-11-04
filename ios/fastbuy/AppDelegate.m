1435bc51246df66c055dea245126c06839cb0acd
dev-cool <cdev9382@gmail.com>
2018-11-04 14:24:37 -0200

    [Added] Google Sign In for iOS

--- ios/fastbuy/AppDelegate.m
+++ ios/fastbuy/AppDelegate.m
@@ -9,6 +9,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
 #import <Firebase.h>
 #import "RCCManager.h"
 
@@ -31,6 +32,13 @@
   [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
   
   return YES;
+}
+  
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application
+                             openURL:url
+                   sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                          annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
 }
 
 @end
