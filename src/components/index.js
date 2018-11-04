9d306cd16968974b7d08cb28cdcbfd8fe414bfe4
dev-cool <cdev9382@gmail.com>
2018-11-03 23:05:42 -0300

    [Added] New components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
+export * from './buttons/Button';
+export * from './buttons/ImageButton';
 export * from './buttons/SocialButton';
+export * from './inputs/Input';
 export * from './items/ProductItem';
+export * from './views/KeyboardView';
