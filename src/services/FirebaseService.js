31baadd13e33495245d86f3296e8da2d73a74b33
dev-cool <cdev9382@gmail.com>
2018-11-04 15:33:16 -0200

    [Added] Firebase sign out

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -2,9 +2,13 @@ import firebase from 'react-native-firebase';
 
 export default class FirebaseService {
 
-  static async signInAndRetrieveData(idToken, accessToken) {
+  static async signIn(idToken, accessToken) {
     const credential = firebase.auth.GoogleAuthProvider.credential(idToken, accessToken);
     return await firebase.auth().signInWithCredential(credential);
+  }
+
+  static async signOut() {
+    return await firebase.auth().signOut();
   }
 
 }
