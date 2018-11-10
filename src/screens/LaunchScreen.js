9d184a691219c68da9c006a2267a40065a374017
dev-cool <cdev9382@gmail.com>
2018-11-10 03:44:35 -0200

    [Added] Loading animation

--- src/screens/LaunchScreen.js
+++ src/screens/LaunchScreen.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import { Animated, StyleSheet, Image, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { tryToSignInSilently } from '../actions';
+import i18n from '../i18n';
 import colors from '../colors';
+import fonts from '../fonts';
 import imgAppIcon from '../../assets/images/app-icon.png';
+
+const PULSE = 0.1;
+const DURATION = 500;
 
 type Props = {};
 class LaunchScreen extends Component<Props> {
+
+  state = {
+    animated: new Animated.Value(1 - PULSE)
+  }
 
   componentWillMount() {
     this.props.tryToSignInSilently();
   }
 
+  componentDidMount() {
+    this.fadeIn();
+  }
+
+  fadeIn() {
+    const { animated } = this.state;
+
+    Animated.timing(animated, {
+      toValue: 1 + PULSE, duration: DURATION,
+    }).start(() => this.fadeOut());
+  }
+
+  fadeOut() {
+    const { animated } = this.state;
+
+    Animated.timing(animated, {
+      toValue: 1 - PULSE, duration: DURATION
+    }).start(() => this.fadeIn());
+  }
+
   render() {
+    const { animated } = this.state;
+
     const {
       containerStyle,
       welcomeViewStyle,
-      imageStyle
+      imageStyle,
+      titleStyle
     } = styles;
 
     return (
       <View style={containerStyle}>
         <View style={welcomeViewStyle}>
-          <Image style={imageStyle} source={imgAppIcon} />
+          <Animated.View style={{ transform: [{ scale: animated }] }}>
+            <Image style={imageStyle} source={imgAppIcon} />
+          </Animated.View>
+
+          <Text style={titleStyle}>{i18n.t('launch.loading')}</Text>
         </View>
       </View>
     );
@@ -46,6 +82,12 @@ const styles = StyleSheet.create({
   imageStyle: {
     height: 100,
     width: 100,
+    margin
+  },
+  titleStyle: {
+    fontFamily: fonts.regular,
+    color: colors.grayDark,
+    fontSize: 24,
     margin
   }
 });
