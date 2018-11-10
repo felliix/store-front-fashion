b9a0936499c7716ad64d1629ae353967ae8906d1
dev-cool <cdev9382@gmail.com>
2018-11-10 02:36:16 -0200

    [Changed] Navigation bar layout

--- src/Router.js
+++ src/Router.js
@@ -1,44 +1,84 @@
-import React from 'react';
-import { Actions, Scene, Router } from 'react-native-router-flux';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Actions, ActionConst, Scene, Stack, Router } from 'react-native-router-flux';
 import i18n from './i18n';
+import colors from './colors';
+import fonts from './fonts';
+import imgAppAdd from '../assets/images/app-add.png';
+import imgAppIcon from '../assets/images/app-back.png';
+import imgAppLogout from '../assets/images/app-logout.png';
 
 import LaunchScreen from './screens/LaunchScreen';
 import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProductsScreen from './screens/ProductsScreen';
 
-const RouterComponent = () => (
-  <Router>
-    <Scene key='root'>
-      <Scene key='mainLaunch' hideNavBar>
-        <Scene key='launch' component={LaunchScreen} />
-      </Scene>
+type Props = {};
+class RouterComponent extends Component<Props> {
 
-      <Scene key='mainLogin' hideNavBar>
-        <Scene key='login' component={LoginScreen} />
-      </Scene>
+  render() {
+    const { sceneStyle, navigationBarStyle, navigationBarTitleStyle } = styles;
+    return (
+      <Router sceneStyle={sceneStyle}>
+        <Stack key='root'>
+          <Stack key='mainLaunch' hideNavBar>
+            <Scene key='launch' component={LaunchScreen} />
+          </Stack>
 
-      <Scene key='main' hideNavBar>
-        <Scene
-          key='products'
-          component={ProductsScreen}
-          title={i18n.t('products.title')}
-          hideNavBar={false}
-          leftTitle='Log Out'
-          onLeft={() => { Actions.mainLaunch(); }}
-          rightTitle='Add'
-          onRight={() => { Actions.product(); }}
-        />
+          <Stack key='mainLogin' type={ActionConst.REPLACE} hideNavBar>
+            <Scene key='login' component={LoginScreen} />
+          </Stack>
 
-        <Scene
-          key='product'
-          component={ProductScreen}
-          title={i18n.t('product.title')}
-          hideNavBar={false}
-        />
-      </Scene>
-    </Scene>
-  </Router>
-);
+          <Stack
+            key='main'
+            navigationBarStyle={navigationBarStyle}
+            titleStyle={navigationBarTitleStyle}
+            headerLayoutPreset='center'
+            backButtonImage={imgAppIcon}
+            type={ActionConst.REPLACE}
+            hideNavBar
+          >
+            <Scene
+              key='products'
+              component={ProductsScreen}
+              title={i18n.t('products.title')}
+              hideNavBar={false}
+              leftButtonImage={imgAppLogout}
+              onLeft={() => { Actions.mainLogin(); }}
+              rightButtonImage={imgAppAdd}
+              onRight={() => { Actions.product(); }}
+            />
+            <Scene
+              key='product'
+              component={ProductScreen}
+              title={i18n.t('product.title')}
+              hideNavBar={false}
+            />
+          </Stack>
+        </Stack>
+      </Router>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  sceneStyle: {
+    backgroundColor: colors.white
+  },
+  navigationBarStyle: {
+    backgroundColor: colors.grayUltraLight,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+    elevation: 3
+  },
+  navigationBarTitleStyle: {
+    color: colors.black,
+    fontFamily: fonts.bold,
+    fontSize: 22
+  }
+});
 
 export default RouterComponent;
--- src/colors.js
+++ src/colors.js
@@ -3,6 +3,7 @@ const colors = {
   gray: '#929292',
   grayDark: '#1F1E1F',
   grayLight: '#E6E6E6',
+  grayUltraLight: '#F7FAFA',
   purple: '#99195E',
   red: '#E74234',
   transparent: 'transparent',
